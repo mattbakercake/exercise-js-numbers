f4e2a86cbe2c0f0c6b2ad269a74c3cd26ac6bedd
Matt Baker <matthew.baker@mikesierra.net>
2016-01-19 19:43:26 +0000

    added form and controller code

--- app/js/views/IndexView.js
+++ app/js/views/IndexView.js
@@ -5,8 +5,9 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/indextemplate.html'
-], function($, _, Backbone, indexTemplate) {
+    'text!templates/indextemplate.html',
+    'js/models/NumberModel'
+], function($, _, Backbone, indexTemplate, Number) {
     /*
      * instantiate the view object
      */
@@ -14,12 +15,40 @@ define([
         compiledTemplate: _.template(indexTemplate), /* define view template */
         el: $('#app-content'), /* define app's DOM output container */
         
-        /*
+        /**
          * render function displays view
          */
         render: function() {
             this.$el.html(this.compiledTemplate()); /* push template content to DOM */
-        }   
+        },
+        
+        /**
+         * register view events 
+         * */
+        events: {
+          'click #submit':'validate'  
+        },
+        
+        /**
+         * checks form input and displays model output
+         */
+        validate: function() {
+            var val = $('#number').val();
+            if (Math.floor(val) == val) {
+                var isInt = true;
+            } else {
+                var isInt = false;
+            }
+            
+            if ($.isNumeric(val) && isInt && val >= 0 && val <= 999999999999) {
+                var number = new Number;
+                var numString = number.getNumString(val);
+                $('#output').html("<span class='num'>" + numString + "</span>");
+            } else {
+                $('#output').html("<span class='error'>Please enter an integer between 0-999999999999</span>");
+            }
+        }
+        
     });
     
     return IndexView; /* return the view object */
